776c87935c694aedc37b359c8f5fd262fa321a67
shimakaze-git <shimakaze.soft+github@googlemail.com>
2018-11-11 15:06:11 +0000

    fix: 環境変数の中身を見るように修正

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -3,6 +3,7 @@ const contentful = require('contentful')
 
 const contentful_config = require('../.contentful.json')
 
+console.log(process.env)
 // use default environment config for convenience
 // these will be set via `env` property in nuxt.config.js
 const config = {
